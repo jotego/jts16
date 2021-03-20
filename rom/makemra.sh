458be5e9e1093479ebc301fdeefea0cf291a8360
Jose Tejada <jotego@gmail.com>
2021-03-20 12:40:49 +0100

    Fantasy Zone

--- rom/makemra.sh
+++ rom/makemra.sh
@@ -9,15 +9,16 @@ function s16a_mra {
     NAME=$1
     FOLDER=$2
     BUTTONS="$3"
-    mkdir -p "$OUTDIR/_alt/$FOLDER"
-    mame2dip $NAME.xml -rbf jts16 -outdir $OUTDIR -altfolder "_alt/$FOLDER" \
+    ALTFOLDER="_alt/_$FOLDER"
+    mkdir -p "$OUTDIR/$ALTFOLDER"
+    mame2dip $NAME.xml -rbf jts16 -outdir $OUTDIR -altfolder "$ALTFOLDER" \
         -header 32 0xFF \
         -setword maincpu 16 reverse \
         -setword sprites 16 reverse \
         -len maincpu   0x40000 \
-        -len n7751data 0x20000 \
-        -len gfx1      0x40000 \
-        -len sprites   0x80000 \
+        -start gfx1    0x68000 \
+        -start sprites 0xa8000 \
+        -len sprites 0x80000 \
         -fill sprites \
         -frac 1 gfx1 4 \
         -order maincpu soundcpu n7751data gfx1 sprites n7751 \
@@ -27,10 +28,12 @@ function s16a_mra {
 }
 
 s16a_mra shinobi   "Shinobi" "Shuriken,Jump,Magic"
-s16a_mra alexkidd  "Alex Kid" "Jump,Shot,Other"
+s16a_mra alexkidd  "Alex Kidd" "Jump,Shot,Other"
 s16a_mra sdi       "SDI" "Fire"
 s16a_mra sjryuko   "Sukeban" "None"
 s16a_mra mjleague  "Major League" "None"
 s16a_mra quartet2a "Quartet" "Jump,Shot,Other"
+s16a_mra afighter  "Action Fighter" "Shot,Button 2,Button 3"
+s16a_mra fantzone  "Fantasy Zone" "Shot,Bomb,Button 3"
 
 exit 0
