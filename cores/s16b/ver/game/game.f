2af1c3e6dfc44526db5bfc0d6f4da992d26a91e3
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 <jose.tejada@jotego.es>
2021-07-11 12:24:35 +0200

    S16B sim files

---
+++ cores/s16b/ver/game/game.f
@@ -0,0 +1,39 @@
+s16a/jts16_game.v
+s16a/jts16_cen.v
+s16a/jts16_video.v
+s16a/jts16_char.v
+s16a/jts16_scr.v
+s16a/jts16_sdram.v
+s16a/jts16_colmix.v
+s16a/jts16_obj.v
+s16a/jts16_obj_ram.v
+s16a/jts16_obj_draw.v
+s16a/jts16_obj_scan.v
+s16a/jts16_mmr.v
+s16a/jts16_pcm.v
+s16a/jts16_fd1089.v
+s16a/jts16_fd1094.v
+s16a/jts16_fd1094_ctrl.v
+s16a/jts16_fd1094_dec.v
+
+../../hdl/jts16b_main.v
+../../hdl/jts16b_snd.v
+
+
+$JTFRAME/hdl/jtframe_sh.v
+$JTFRAME/hdl/jtframe_ff.v
+$JTFRAME/hdl/clocking/jtframe_frac_cen.v
+$JTFRAME/hdl/video/jtframe_vtimer.v
+$JTFRAME/hdl/video/jtframe_blank.v
+$JTFRAME/hdl/video/jtframe_linebuf.v
+$JTFRAME/hdl/ram/jtframe_ram.v
+$JTFRAME/hdl/ram/jtframe_prom.v
+$JTFRAME/hdl/ram/jtframe_dual_ram.v
+$JTFRAME/hdl/ram/jtframe_dual_ram16.v
+$JTFRAME/hdl/ram/jtframe_obj_buffer.v
+$JTFRAME/hdl/sound/jtframe_mixer.v
+$JTFRAME/hdl/sdram/jtframe_dwnld.v
+-F $JTFRAME/hdl/sdram/jtframe_sdram64.f
+
+# 68000
+-F $JTFRAME/hdl/cpu/jtframe_m68k.f
---
+++ cores/s16b/ver/game/sim.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+SYSNAME=s16b
+GAME=altbeast5
+SCENE=
+OTHER=
+HEXDUMP=-nohex
+SIMULATOR=-verilator
+SDRAM_SNAP=
+
+AUXTMP=/tmp/$RANDOM$RANDOM
+jtcfgstr -target=mist -output=bash -parse ../../hdl/jts16b.def |grep _START > $AUXTMP
+source $AUXTMP
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        -g)
+            shift
+            GAME=$1
+            if [ ! -e $ROM/$GAME.rom ]; then
+                echo "Cannot find ROM file $ROM/$GAME.rom"
+                exit 1
+            fi
+            ;;
+    	-s|-scene)
+            shift
+    		SCENE=$1;;
+    	*)
+            OTHER="$OTHER $1";;
+    esac
+    shift
+done
+
+ln -sf $ROM/$GAME.rom rom.bin
+
+if [ ! -z "$SCENE" ]; then
+	echo "Simulating scene $SCENE"
+
+	if [ ! -e $GAME/char$SCENE.bin ]; then
+	    echo Cannot open scene files
+	    exit 1
+	fi
+
+	drop1    < $GAME/char${SCENE}.bin > char_hi.bin
+	drop1 -l < $GAME/char${SCENE}.bin > char_lo.bin
+
+	drop1    < $GAME/pal${SCENE}.bin > pal_hi.bin
+	drop1 -l < $GAME/pal${SCENE}.bin > pal_lo.bin
+
+	drop1    < $GAME/obj${SCENE}.bin > obj_hi.bin
+	drop1 -l < $GAME/obj${SCENE}.bin > obj_lo.bin
+
+    hexdump -v -e '/1 "%02X "' -s 0xe00 $GAME/char${SCENE}.bin > mmr.hex
+
+	cp $GAME/scr${SCENE}.bin scr.bin
+    OTHER="$OTHER -d NOMAIN -video -nosnd"
+    if [[ ! "$*" =~ -time ]]; then
+        OTHER="$OTHER -time 16"
+    fi
+    SDRAM_SNAP="-snap scr.bin 0 0x200000"
+else
+    rm -f char_*.bin pal_*.bin obj_*.bin scr.bin
+fi
+
+if which ncverilog >/dev/null; then
+    # Options for non-verilator simulation
+    SIMULATOR=
+    HEXDUMP=
+fi
+
+rm -f sdram_bank?.*
+jtsim_sdram $HEXDUMP -header 32 \
+    -banks $BA1_START $BA2_START $BA3_START \
+    -stop $MCU_START \
+    -dumpbin 317-5021.key $MAINKEY_START 0x2000 \
+    -dumpbin fd1089.bin   $FD1089_START  0x0100 \
+    $SDRAM_SNAP || exit $?
+
+jtsim_sdram -header 32 -dumpbin fd1094.bin 0x182000 8192
+
+jtsim -mist -sysname $SYSNAME $SIMULATOR \
+	-videow 320 -videoh 224 \
+    -def ../../hdl/jts16.def -d JTFRAME_SIM_ROMRQ_NOCHECK $OTHER || exit $?
+
+if [[ ! -z "$SCENE" && -e frame_1.jpg ]]; then
+	eom frame_1.jpg 2> /dev/null
+fi
