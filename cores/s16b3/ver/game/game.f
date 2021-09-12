06c95acc74d36f55abebfb1dbbc2ff05806f3cb5
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 <jose.tejada@jotego.es>
2021-09-12 15:34:48 +0200

    wip

---
+++ cores/s16b3/ver/game/game.f
@@ -0,0 +1,42 @@
+$CORES/s16/hdl/jts16_game.v
+$CORES/s16/hdl/jts16_cen.v
+$CORES/s16/hdl/jts16_video.v
+$CORES/s16/hdl/jts16_char.v
+$CORES/s16/hdl/jts16_scr.v
+$CORES/s16/hdl/jts16_sdram.v
+$CORES/s16/hdl/jts16_colmix.v
+$CORES/s16/hdl/jts16_obj.v
+$CORES/s16/hdl/jts16_obj_ram.v
+$CORES/s16/hdl/jts16_obj_draw.v
+$CORES/s16/hdl/jts16_obj_scan.v
+$CORES/s16/hdl/jts16_mmr.v
+$CORES/s16/hdl/jts16_pcm.v
+$CORES/s16/hdl/jts16_fd1089.v
+$CORES/s16/hdl/jts16_fd1094.v
+$CORES/s16/hdl/jts16_fd1094_ctrl.v
+$CORES/s16/hdl/jts16_fd1094_dec.v
+
+$CORES/s16b/hdl/jts16b_main.v
+$CORES/s16b/hdl/jts16b_mapper.v
+$CORES/s16b/hdl/jts16b_snd.v
+$CORES/s16b/hdl/jts16_mult.v
+
+
+$JTFRAME/hdl/jtframe_sh.v
+$JTFRAME/hdl/jtframe_ff.v
+$JTFRAME/hdl/clocking/jtframe_frac_cen.v
+$JTFRAME/hdl/video/jtframe_vtimer.v
+$JTFRAME/hdl/video/jtframe_blank.v
+$JTFRAME/hdl/video/jtframe_linebuf.v
+$JTFRAME/hdl/ram/jtframe_ram.v
+$JTFRAME/hdl/ram/jtframe_prom.v
+$JTFRAME/hdl/ram/jtframe_dual_ram.v
+$JTFRAME/hdl/ram/jtframe_dual_ram16.v
+$JTFRAME/hdl/ram/jtframe_obj_buffer.v
+$JTFRAME/hdl/sound/jtframe_mixer.v
+$JTFRAME/hdl/sound/jtframe_pole.v
+$JTFRAME/hdl/sdram/jtframe_dwnld.v
+-F $JTFRAME/hdl/sdram/jtframe_sdram64.f
+
+# 68000
+-F $JTFRAME/hdl/cpu/jtframe_m68k.f
