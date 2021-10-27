jotego/jts16

commit 62443aa27487546c1dbd2e3a54d7d901f2870fa7

Fixes issue #66 Cotton sprites disappearing

cores/s16/hdl/jts16_obj_scan.v
@@ -89,6 +89,9 @@ wire       badobj = top >= bottom;
     initial zoom = 0;
 `endif
 
+`ifdef SIMULATION
+    wire hide = MODEL && st==3 && tbl_dout[14];
+`endif
 
 always @(posedge clk, posedge rst) begin
     if( rst ) begin
@@ -124,19 +127,21 @@ always @(posedge clk, posedge rst) begin
             1: begin
                 if( !stop ) begin
                     visible <= inzone;
-                    if( bottom>=8'hf0 ) begin
-                        st <= 0; // Done
-                    end else if( MODEL==0 && (!inzone || badobj) ) begin
-                        // For S16A there is no end-of-table (EOT) bit, so we
-                        // can loop around here. For S16B, we need to check
-                        // tbl_dout at state 3 for the EOT bit
-                        // Next object
-                        cur_obj <= cur_obj + 1'd1;
-                        idx     <= 0;
-                        st      <= 1;
-                        stop    <= 1;
-                        if( &cur_obj )
-                            st <= 0; // we're done
+                    if( MODEL==0 ) begin // System 16A only
+                        if( bottom>=8'hf0 ) begin
+                            st <= 0; // Done
+                        end else if( !inzone || badobj ) begin
+                            // For S16A there is no end-of-table (EOT) bit, so we
+                            // can loop around here. For S16B, we need to check
+                            // tbl_dout at state 3 for the EOT bit
+                            // Next object
+                            cur_obj <= cur_obj + 1'd1;
+                            idx     <= 0;
+                            st      <= 1;
+                            stop    <= 1;
+                            if( &cur_obj )
+                                st <= 0; // we're done
+                        end
                     end else begin // draw this one
                         first <= top == vrf[7:0]; // first line
                     end

commit efec4317e7a9b1f40edfea17d4269263d0ac691d

Fixes S16A sprites, broken in 62443aa

cores/s16/hdl/jts16_obj_scan.v
@@ -127,21 +127,19 @@ always @(posedge clk, posedge rst) begin
             1: begin
                 if( !stop ) begin
                     visible <= inzone;
-                    if( MODEL==0 ) begin // System 16A only
-                        if( bottom>=8'hf0 ) begin
-                            st <= 0; // Done
-                        end else if( !inzone || badobj ) begin
-                            // For S16A there is no end-of-table (EOT) bit, so we
-                            // can loop around here. For S16B, we need to check
-                            // tbl_dout at state 3 for the EOT bit
-                            // Next object
-                            cur_obj <= cur_obj + 1'd1;
-                            idx     <= 0;
-                            st      <= 1;
-                            stop    <= 1;
-                            if( &cur_obj )
-                                st <= 0; // we're done
-                        end
+                    if( MODEL==0 && bottom>=8'hf0 ) begin
+                        st <= 0; // Done
+                    end else if( MODEL==0 && (!inzone || badobj) ) begin
+                        // For S16A there is no end-of-table (EOT) bit, so we
+                        // can loop around here. For S16B, we need to check
+                        // tbl_dout at state 3 for the EOT bit
+                        // Next object
+                        cur_obj <= cur_obj + 1'd1;
+                        idx     <= 0;
+                        st      <= 1;
+                        stop    <= 1;
+                        if( &cur_obj )
+                            st <= 0; // we're done
                     end else begin // draw this one
                         first <= top == vrf[7:0]; // first line
                     end